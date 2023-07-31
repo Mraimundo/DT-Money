31b84c4214267577eaa308a36793de4814164da6
Mraimundo <mdumundo18@gmail.com>
2023-07-31 15:17:01 -0300

    feat: Add Transaction Table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summery";
+import { TransactionContainer, TransactionTable, PriceHighlight } from "./styled";
+
 
 export function Transactions() {
   return (
     <div>
       <Header/>
       <Summary/>
+
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvedor de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>vendas</td>
+              <td>13/06/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hanburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  -R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>14/06/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
   )
 }
